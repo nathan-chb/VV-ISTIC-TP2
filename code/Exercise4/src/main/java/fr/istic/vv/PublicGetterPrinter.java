03744fea3424e3d7871df67c0e3dc45a7a595f37
Nathan Chambron <nathanchambron@yahoo.fr>
2022-03-09 16:32:45 +0100

    changed to check if getter returns the value

--- code/Exercise4/src/main/java/fr/istic/vv/PublicGetterPrinter.java
+++ code/Exercise4/src/main/java/fr/istic/vv/PublicGetterPrinter.java
@@ -2,6 +2,7 @@ package fr.istic.vv;
 
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.visitor.VoidVisitorWithDefaults;
 import com.github.javaparser.utils.Pair;
 
@@ -57,25 +58,40 @@ public class PublicGetterPrinter extends VoidVisitorWithDefaults<Void> {
 
     @Override
     public void visit(MethodDeclaration declaration, Void arg) {
+
         if(!declaration.isPublic()) return;
         Iterator<Map.Entry<FieldDeclaration, Pair<String,String>>> iter = privateFieldsWithContext.entrySet().iterator();
         while (iter.hasNext()) {
             Map.Entry<FieldDeclaration, Pair<String,String>> entry = iter.next();
             String fieldString = entry.getKey().getVariable(0).getNameAsString();
             String capitalizedField = fieldString.substring(0, 1).toUpperCase() + fieldString.substring(1);
+
+            //check if the name of the method is get<name-of-field> with a capitalized first letter
             if (declaration.getNameAsString().startsWith("get" + capitalizedField)) {
-                iter.remove();
+
+                //check if the method returns the field
+                if (declaration.getBody().isPresent()) {
+                    for (Statement statement : declaration.getBody().get().getStatements()) {
+                        if ((statement.toString().equalsIgnoreCase("return " + fieldString + ";")) || (statement.toString().equalsIgnoreCase("return this." + fieldString + ";"))) {
+                            System.out.println("statement " + statement.toString());
+                            iter.remove();
+                            break;
+                        }
+                    }
+                }
             }
         }
     }
 
     @Override
     public void visit(FieldDeclaration declaration, Void arg) {
+        //We put all the private fields in a map with its class and package
         if(!declaration.isPublic()){
             privateFieldsWithContext.put(declaration, new Pair<>(currentClass, currentPackage));
         }
     }
 
+    //method to export our private fields without getter to a csv file
     public void export() throws IOException {
         List<String[]> dataLines = new ArrayList<>();
         for (Map.Entry<FieldDeclaration, Pair<String,String>> entry : privateFieldsWithContext.entrySet()) {
@@ -92,6 +108,7 @@ public class PublicGetterPrinter extends VoidVisitorWithDefaults<Void> {
         }
     }
 
+    // method to convert data to CSV data
     public String convertToCSV(String[] data) {
         return Stream.of(data)
                 .collect(Collectors.joining(","));
